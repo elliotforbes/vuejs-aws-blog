5d056f602296f4e23c21a42e0c9c67ec0866cd2f
Elliot Forbes <emforce@live.com>
2018-04-18 19:37:53 +0100

    Updated frontend deployment script

--- deploy/frontend.sh
+++ deploy/frontend.sh
@@ -1,8 +1,13 @@
-echo 'frontend'
-echo 'it works'
-npm -version
+# navigate into our frontend directory
 cd frontend/
+
+# build our frontend project
+npm install
 npm run build
+
+# navigate into the directory which contains
+# our built frontend
 cd dist/
+
+# Push to S3
 ls
-pwd
